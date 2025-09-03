20a6f23bd57347c59b9a3d35de4a6bcd3e14ab69
Pranpriya <pranpriya.khe@dome.tu.ac.th>
2025-09-03 18:16:19 +0700

    one

---
+++ testMicroservice1.zip_expanded/testmicroservice1/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/greeting")
+	public ResponseEntity<?> greeting(){
+		return ResponseEntity.ok("Hello");
+	}
+
+}
